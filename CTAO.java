0162528cbe197e219d077052fca88c2cb02198ac
atheeeraalsaif <atheeeraalsaif@hotmail.com>
2016-04-25 11:23:44 +0300

    Update CTAO.java

--- CTAO.java
+++ CTAO.java
@@ -2,10 +2,31 @@ public class CTAO{
 
 
 
-public static int encrypt(int key, int plain){}
+for(int i=1; i<65536; i++){
+
+String or char testKey;
+
+int	c = Hex16.convert(s);
+int	p = Coder.decrypt(i, c);
 
 
-public static  int decrypt(int key, int cipher){}
+//check the dycrepted message
+boolean flag= true;
+while(flag){
+for(n=0; n<26; n++)
+if (!Character.isLetter(p.charAt(n))){
+flag=false;
+
+}}
+
+
+if(flag){
+String key=testKey;
+}
+
+}
+
+
 
 
 }
