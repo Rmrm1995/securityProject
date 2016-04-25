4c4ac556c917e0cec9aac13519926811e580b616
Rmrm1995 <rmrm1995@gmail.com>
2016-04-25 16:28:04 +0300

    Update CTAO.java

--- CTAO.java
+++ CTAO.java
@@ -1,10 +1,27 @@
 public class CTAO{
 
+//Atheer & Nuha we can use two ways either reading the content of the text file by using a file reader or 
+//storing file content in an array but I think the 1st way is waayyy better :)
 
-
+//2^16=65536 "range of keys" we dont need 0 
 for(int i=1; i<65536; i++){
 
-String or char testKey;
+public static void readLinesUsingFileReader() throws IOException 
+{
+    File file = new File("Security.txt");
+    FileReader fr = new FileReader(file);
+    BufferedReader br = new BufferedReader(fr);
+    String line;
+    while((line = br.readLine()) != null){
+        
+        }
+    }
+    br.close();
+    fr.close();
+}
+
+
+/*String or char testKey;
 
 int	c = Hex16.convert(s);
 int	p = Coder.decrypt(i, c);
@@ -22,7 +39,7 @@ flag=false;
 
 if(flag){
 String key=testKey;
-}
+}*/
 
 }
 
